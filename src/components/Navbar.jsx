eef05b515605697e6d91d9492993c58ce065daa8
Risheb.s <rishebs123456@gmail.com>
2025-10-18 22:19:21 +0530

    added stepper form function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -90,7 +90,9 @@ export default function Navbar() {
           >
             {/* Header */}
             <div className="flex items-center justify-between px-6 py-5 border-b border-white/40">
-              <span className="font-melody text-3xl text-orange-600">yatrasutra</span>
+              <Link to="/" className="flex items-center gap-2" onClick={() => setOpen(false)}>
+            <span className="font-melody text-3xl text-orange-600">yatrasutra</span>
+          </Link>
               <button onClick={() => setOpen(false)} aria-label="Close" className="rounded-md p-3 text-slate-700 hover:bg-white/60">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-7 w-7">
                   <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 11-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
